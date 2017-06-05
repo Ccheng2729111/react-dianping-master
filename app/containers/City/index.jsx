bf84d8f08d1ec581a6b0e88160c7ecd67525020b
ChengJian <53271286@qq.com>
2017-06-05 17:03:41 +0800

    选择城市功能完成。

--- app/containers/City/index.jsx
+++ app/containers/City/index.jsx
@@ -5,6 +5,10 @@ import * as userInfoActionFromOther from '../../actions/userinfo'
 import {bindActionCreators} from 'redux'
 import Header from '../../components/Header/index'
 import CurrentCity from '../../components/CurrentCity/index'
+import CityList from '../../components/CityList/index'
+import localStore from '../../util/localStore'
+import {CITYNAME} from '../../config/localStoreKey'
+import {hashHistory} from 'react-router'
 
 class City extends React.Component {
     constructor(props, context) {
@@ -16,11 +20,15 @@ class City extends React.Component {
             <div>
                 <Header title="选择城市" />
                 <CurrentCity cityName={this.props.userinfo.cityName}/>
+                <CityList chengeCityHandler = {this.changeCityFn.bind(this)}/>
             </div>
         )
     }
-    componentDidMount(){
-        console.log(this.props.userInfoActions)
+    changeCityFn(newCity){
+        const userinfo = this.props.userinfo//获取redux中存储的userinfo
+        userinfo.cityName = newCity          //将userinfo里的cityname改成传入的值
+        localStore.setItem(CITYNAME,newCity)//将localStore改成新的城市
+        hashHistory.push('/')               //使用route的hashHistory返回首页。
     }
 }
 function mapStateToProps(state){
